c5dd846aa10a206f76530943489d9ca561da798b
Ketlyn dos Santos Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-04-09 08:17:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const textos = document.querySelectorAll('.aba-conteudo')
 
 const contadores = document.querySelectorAll('.contador')
 
-const tempoObjetivo01 = new Date("2024-04-03T00:00:00")
-const tempoObjetivo02 = new Date("2024-06-25T00:00:00")
-const tempoObjetivo03 = new Date("2024-07-15T00:00:00")
-const tempoObjetivo04 = new Date("2024-07-06T00:00:00")
+const tempoObjetivo01 = new Date("2024-04-15T00:00:00")
+const tempoObjetivo02 = new Date("2024-11-30T00:00:00")
+const tempoObjetivo03 = new Date("2025-02-01T00:00:00")
+const tempoObjetivo04 = new Date("2025-02-01T00:00:00")
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
